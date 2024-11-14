8ed7249fcb7634c43a41e37fbecf0789c2a19bc0
Petter Daae <petter.daae@gmail.com>
2024-11-14 11:15:57 +0100

    Convert config beans to kotlin

---
+++ src/main/kotlin/dev/daae/time/config/ClockConfig.kt
@@ -0,0 +1,13 @@
+package dev.daae.time.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import java.time.Clock
+
+@Configuration
+open class ClockConfig {
+    @Bean
+    open fun clock(): Clock {
+        return Clock.systemDefaultZone()
+    }
+}
